4befe12d0b36b8df305ad7d9d1e60074bb27c4e2
alexiskov <alexiskov.rus@gmail.com>
2024-10-02 14:10:18 +0300

    url parsing methods added

--- htpsrv/bin.go
+++ htpsrv/bin.go
@@ -2,7 +2,11 @@ package htpsrv
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/url"
+	"strconv"
+	"time"
 )
 
 func New(port uint16) ServerEntity {
@@ -16,5 +20,55 @@ func (srv ServerEntity) Start() (err error) {
 }
 
 func router(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		getProcessing(w, r)
+	}
+	if r.Method == http.MethodPut {
+
+	}
+	if r.Method == http.MethodPost {
+
+	}
+	if r.Method == http.MethodDelete {
+
+	}
+}
+
+func getProcessing(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		log.Println(err)
+	}
+
+	switch u.Path {
+	case "/info":
+		v := u.Query()
+
+		params := URLQueryParamsEntity{Artist: v.Get("group"), Song: v.Get("song"), TextFragment: v.Get("textFragment")}
+
+		rd := v.Get("releaseDate")
+		if rd != "" {
+			var t time.Time
+			t, err := time.Parse("02.01.2006", rd)
+			if err != nil {
+				err = fmt.Errorf("query param time parsing error: %w", err)
+				log.Println(err)
+				return
+			}
+			params.ReleaseDate = t.Unix()
+		}
+
+		p := v.Get("page")
+		if p != "" {
+			pi, err := strconv.Atoi(p)
+			if err != nil {
+				log.Println(err)
+			}
+			params.Page = uint64(pi)
+		}
+
+	case "/geText":
+
+	}
 
 }
--- htpsrv/obj.go
+++ htpsrv/obj.go
@@ -6,4 +6,17 @@ type (
 	ServerEntity struct {
 		Http *http.Server
 	}
+
+	URLQueryParamsEntity struct {
+		Artist       string
+		Song         string
+		ReleaseDate  int64
+		TextFragment string
+		Page         uint64
+	}
+
+	URLQuerySongParamsEntity struct {
+		SongID uint
+		Page   uint
+	}
 )
